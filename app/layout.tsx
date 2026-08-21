f84f35cecb001af9b4209d8f3f0d3f4e7df8a14a
MuazaliMv <90874558+MuazaliMv@users.noreply.github.com>
2026-08-21 13:56:13 +0500

    Load dynamic UI theme at runtime

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import ThemeRuntime from './ThemeRuntime';
 
 export const metadata: Metadata = {
   title: 'FixIt Maldives',
@@ -9,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body><ThemeRuntime/>{children}</body>
     </html>
   );
 }
