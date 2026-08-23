23bd9bbf8c4f30452a82c77cc6c93ccf0389297a
MuazaliMv <90874558+MuazaliMv@users.noreply.github.com>
2026-08-23 19:56:53 +0500

    Force dynamic profile route

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,8 @@
 import ProfileClient from './ProfileClient';
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export default function ProfilePage(){
   return <ProfileClient/>;
 }
