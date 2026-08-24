50fb8d6d96089da39fc7d24f1f2d466f56777938
MuazaliMv <90874558+MuazaliMv@users.noreply.github.com>
2026-08-25 03:40:07 +0500

    Remove duplicate profile renderer

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,12 +1,8 @@
 import ProfileClient from './ProfileClient';
-import ProfileDataEntryCard from './ProfileDataEntryCard';
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 export default function ProfilePage(){
-  return <>
-    <div className="shell accountApp"><ProfileDataEntryCard/></div>
-    <ProfileClient/>
-  </>;
+  return <ProfileClient/>;
 }
